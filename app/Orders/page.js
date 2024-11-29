0903e66f59cd590c7bcefb8e19198d78575670f9
Muhammad Idrees <muhammadidr1122@gmail.com>
2024-11-29 10:10:45 -0800

    Update page.js

--- app/Orders/page.js
+++ app/Orders/page.js
@@ -34,7 +34,7 @@ const page = () => {
         <tbody>
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
+                    Apple MacBook Pro 17
                 </th>
                 <td class="px-6 py-4">
                     Silver
